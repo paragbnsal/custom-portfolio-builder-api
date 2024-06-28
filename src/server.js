d9a13024c9505e7c154c5a958b4fd59774eb0f7d
paragbansl <parag@clearly.earth>
2024-06-29 01:05:11 +0530

    routes and controller setup with healthcheck

--- src/server.js
+++ src/server.js
@@ -2,6 +2,9 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
+// Import Controller
+import healthCheckRouter from "./routes/health.routes.js";
+
 const app = express();
 
 // Init Middlewares
@@ -10,5 +13,8 @@ app.use(express.static("public"));
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(cookieParser());
+
+// Init Controllers
+app.use("/health", healthCheckRouter);
 
 export default app;
