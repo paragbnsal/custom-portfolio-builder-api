84a609d0886c5568c2b193fec0c159c0ce40b54f
paragbansl <parag@clearly.earth>
2024-06-30 00:51:32 +0530

    configured logging

--- src/server.js
+++ src/server.js
@@ -1,6 +1,10 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import logger from "./utils/logger.js";
+import morgan from "morgan";
+
+const morganFormat = ":method :url :status :response-time ms :res[content-length] bytes";
 
 // Import Controller
 import healthCheckRouter from "./routes/health.routes.js";
@@ -13,6 +17,22 @@ app.use(express.static("public"));
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(cookieParser());
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: `${message.split(" ")[3]} ms`,
+          responseSize: `${message.split(" ")[5]} bytes`,
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
 
 // Init Controllers
 app.use("/health", healthCheckRouter);
